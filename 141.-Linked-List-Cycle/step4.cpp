9858cfcd91bf1301a6ae356f0dbbf6802077d685
maeken4 <kengo6924@gmail.com>
2025-05-11 05:24:08 +0900

    PRを受け修正

---
+++ 141.-Linked-List-Cycle/step4.cpp
@@ -0,0 +1,24 @@
+// PRを受け修正
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+    public:
+    bool hasCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};

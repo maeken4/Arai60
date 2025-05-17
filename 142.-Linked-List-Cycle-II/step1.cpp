2d05403fd81a27c68d1b2bea7698576f42249f6a
maeken4 <kengo6924@gmail.com>
2025-05-18 01:43:38 +0900

    最終行に改行を追加

--- 142.-Linked-List-Cycle-II/step1.cpp
+++ 142.-Linked-List-Cycle-II/step1.cpp
@@ -1,25 +1,25 @@
-#include<set>
+#include <set>
 
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr){}
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
 };
 
 // 25m
 class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        std::set<ListNode*> visited;
+   public:
+	ListNode *detectCycle(ListNode *head) {
+		std::set<ListNode *> visited;
 
-        while (head != nullptr) {
-            visited.insert(head);
-            head = head->next;
-            // findにnullptr入れても大丈夫なんだっけ？
-            if(visited.find(head) != visited.end()){
-                return head;
-            }
-        }
-        return nullptr;
-    }
+		while (head != nullptr) {
+			visited.insert(head);
+			head = head->next;
+			// findにnullptr入れても大丈夫なんだっけ？
+			if (visited.find(head) != visited.end()) {
+				return head;
+			}
+		}
+		return nullptr;
+	}
 };

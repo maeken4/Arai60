0df0aa780424fc413db0f486d4aee3b3bc3e3c88
maeken4 <kengo6924@gmail.com>
2025-05-24 19:21:20 +0900

    redo step3

--- 83.-Remove-Duplicates-from-Sorted-List/step3_another.cpp
+++ 83.-Remove-Duplicates-from-Sorted-List/step3_another.cpp
@@ -1,26 +1,30 @@
 struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode() : val(0), next(nullptr) {}
-	ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode* next) : val(x), next(next) {}
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
    public:
-	ListNode* deleteDuplicates(ListNode* head) {
-		ListNode* nowNode = head;
-		ListNode* resultHead = nowNode ? new ListNode(nowNode->val) : nullptr;
-		ListNode* resultTail = resultHead;
-		while (nowNode) {
-			if (nowNode->next == nullptr) {
-				break;
-			} else if (nowNode->val != nowNode->next->val) {
-				ListNode* append = new ListNode(nowNode->next->val);
-			}
-			// 次のノードも同じ値の場合は何も追加せずに進む
-			nowNode = nowNode->next;
-		}
-		return resultHead;
-	}
+    // 入力のリストも戻り値のリストもメモリ管理は呼び出し元で行う必要がある。
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* nowNode = head;
+        ListNode* resultHead = nowNode ? new ListNode(nowNode->val) : nullptr;
+        ListNode* resultTail = resultHead;
+        while (nowNode != nullptr) {
+            if (nowNode->next == nullptr) {
+                break;
+                // 次のノードの値が異なる値であれば戻り値のリストに追加する
+            } else if (nowNode->val != nowNode->next->val) {
+                ListNode* appendNode = new ListNode(nowNode->next->val);
+                resultTail->next = appendNode;
+                resultTail = resultTail->next;
+            }
+            // 次のノードも同じ値の場合は何も追加せずに進んでよい
+            nowNode = nowNode->next;
+        }
+        return resultHead;
+    }
 };
